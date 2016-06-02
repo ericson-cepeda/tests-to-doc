3979671bd3b23de0cf625740b9c8df2424fe8060
ignusmart <ignusmart@gmail.com>
2016-06-01 22:50:59 -0500

    push all the found repos with all branches to bitBucket

--- scripts/check_git_repos.py
+++ scripts/check_git_repos.py
@@ -1,73 +1,88 @@
 import sys
 import os
 import argparse
+import requests
 from git import Repo, cmd
-from bitbucket.bitbucket import Bitbucket
+
+# owner can be the user or team
+def create_bitbucket_repo(user, pwd, owner, repo_slug):
+  data = {"scm": "git", "is_private": "true", "fork_policy": "no_public_forks"} 
+  url = "https://api.bitbucket.org/2.0/repositories/%s/%s"%(owner, repo_slug)
+  headers = {"Content-Type": "application/json"}
+  response = requests.post(url, json=data,headers=headers, auth=(user, pwd))
 
 def get_repos_dirs(tree_dir):
-	# repos dirs
-	repos_dirs = []
+  # repos dirs
+  repos_dirs = []
 
-	subdirs = [x[0] for x in os.walk(tree_dir)]
-	for subdir in subdirs:
-		try: 
-			repo = Repo(subdir)
-			# if subfolder does not have the sufix .git then add it
-			if(subdir.split('.')[-1] != 'git'):
-				repos_dirs.append(subdir)
+  subdirs = [x[0] for x in os.walk(tree_dir)]
+  for subdir in subdirs:
+    try: 
+      repo = Repo(subdir)
+      # if subfolder does not have the sufix .git then add it
+      if(subdir.split('.')[-1] != 'git'):
+        repos_dirs.append(subdir)
 
-		except:
-			pass
+    except:
+      pass
 
-	return repos_dirs
+  return repos_dirs
 
 def check_git_repos(cmdargs):
-	# path gived by user
-	root = cmdargs.path
+  # path gived by user
+  root = cmdargs.path
 
-	# Bitbucket crendentials
-	pwd = cmdargs.pwd
-	user = cmdargs.pwd
+  # Bitbucket crendentials
+  pwd = cmdargs.pwd
+  user = cmdargs.user
+  owner = cmdargs.owner
 
-	# Bitbucket Login
-	bb = Bitbucket(user, pwd)
+  if(os.path.exists(cmdargs.path)):
+    formated_root = os.path.abspath(root)
 
-	if(os.path.exists(cmdargs.path)):
-		formated_root = os.path.abspath(root)
+    #get the repos dirs
+    repo_dirs = get_repos_dirs(formated_root)
 
-		#get the repos dirs
-		repo_dirs = get_repos_dirs(formated_root)
+    for repo_dir in repo_dirs:
+      # get the repo name from remote origin url as <name>.git
+      g = cmd.Git(repo_dir)
+      repo_name = g.execute(["git", "config", "--get", "remote.origin.url"]).split('/')[-1]
 
-		for repo_dir in repo_dirs:
-			# generate the URL based on folder 
-			g = cmd.Git(repo_dir)
-			
-			# get the repo name from remote origin url as <name>.git
-			repo_name = g.execute(["git", "config", "--get", "remote.origin.url"]).split('/')[-1]
+      # @TODO: Maybe picorb could be replaced with a sysargv
+      url = 'git@bitbucket.org:%s/%s'%(owner,repo_name)
 
-			# @TODO: Maybe picorb could be replaced with a sysargv
-			url = 'git@bitbucket.org:picorb/%s'%(repo_name)
+      repo = Repo(repo_dir)
 
-			repo = Repo(repo_dir)
-			# check if repo has uncommited changes
-			if (repo.is_dirty()):
-				remote_check = repo.create_remote('check_repo',url)
-				# @TODO: Implement this 
-				repo.delete_remote(remote_check)
+      # create a temporal remote
+      bitbucket_remote = repo.create_remote('bitbucket_remote',url)
+      # create the bitbucket repo
+      create_bitbucket_repo(user, pwd, owner, repo_name.split(".")[0])
 
-	else:
-		sys.stderr.write('Please enter a valid path. \n')
+      # Push all branches to the new remote
+      g.execute(["git", "push", 'bitbucket_remote', "--all"])
+
+      # check if repo has uncommited changes
+      if (repo.is_dirty()):
+        # @TODO: Commit those changes on a new `uncommited`branch and push
+        pass
+
+      # delete temporal remote
+      repo.delete_remote(bitbucket_remote)
+
+  else:
+    sys.stderr.write('Please enter a valid path. \n')
 
 
 def main(argv=None):
-	description = "This script check git repositories into the gived path and upload them to bitbucket"
-	parser = argparse.ArgumentParser(description=description, add_help=True)
-	parser.add_argument('-path', dest='path', type=str, help='The path to check', required=True)
-	parser.add_argument('-user', dest='user', type=str, help='Bitbucket username', required=True)
-	parser.add_argument('-pwd', dest='pwd', type=str, help='Bitbucket password', required=True)
-	cmdargs = parser.parse_args()
-	check_git_repos(cmdargs)
-	
+  description = "This script check git repositories into the gived path and upload the dirty repos to bitbucket"
+  parser = argparse.ArgumentParser(description=description, add_help=True)
+  parser.add_argument('-path', dest='path', type=str, help='The path to check', required=True)
+  parser.add_argument('-user', dest='user', type=str, help='Bitbucket username', required=True)
+  parser.add_argument('-pwd', dest='pwd', type=str, help='Bitbucket password', required=True)
+  parser.add_argument('-owner', dest='owner', type=str, help='Bitbucket team or owner', required=True)
+  cmdargs = parser.parse_args()
+  check_git_repos(cmdargs)
+  
 
 if __name__ == '__main__':
   status = main(sys.argv[1:])
